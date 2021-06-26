b453b5ae7aa7dceaa7746b331e5ad18e919dd511
Yasser-ui <yasser.benyoussef@isigk.u-kairouan.tn>
2021-06-26 18:05:59 +0100

    First Commit

---
+++ config/config.js
@@ -0,0 +1,16 @@
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    mongoUri: process.env.MONGODB_URI ||
+      process.env.MONGO_HOST ||
+      'mongodb://' + (process.env.IP || "localhost") + ':' +
+      (process.env.MONGO_PORT || '27017') +
+      '/webMarket',
+    stripe_connect_test_client_id: 'cus_Jj13abtqVne5zO',
+    stripe_test_secret_key: 'sk_test_51J5YaSGFxKsI4hRj89X72y0rh7jzomtjqCMhT5rJJJuoTqdTOetn3IaVpuV521GCbWXY0fM4wzw1RNYfHuEqlHnD00oSi3qcMa',
+    stripe_test_api_key: 'pk_test_51J5YaSGFxKsI4hRj7fvSsyvOuodmHv9qXd5S7HVZDD9dFZBLaNhEzzc9izrTjlYHpSlJz8Fc2f2BzPyrWVhDExo700kJb5GZnZ'
+  }
+  
+  export default config
+  
---
+++ server/server.js
@@ -0,0 +1,20 @@
+import config from './../config/config'
+import app from './express'
+import mongoose from 'mongoose'
+import bidding from './controllers/bidding.controller'
+
+// connection URL
+mongoose.Promise = global.Promise
+mongoose.connect(/*config.mongoUrl*/"mongodb://localhost:27017/webMarket", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true})
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: ${config.mongoUrl}`)
+})
+
+const server = app.listen(config.port, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.info('Server started on port %s.', config.port)
+})
+
+bidding(server)
